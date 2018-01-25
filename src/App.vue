0259331832a889beab1aafb4a661e677b62ebf60
R10burning <790309793@qq.com>
2018-01-25 22:12:34 +0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: red;
   margin-top: 60px;
 }
 </style>
